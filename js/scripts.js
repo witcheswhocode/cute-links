5ec0c77e0332cf3fe34bf0b1aa29e4031a137874
liz <lizanderson.ca@gmail.com>
2022-03-05 00:08:26 -0800

    more style tweaks, add my projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,17 @@
 const links = [{
-    "link": "https://twitter.com/witcheswhocode",
-    "title": "Twitter",
-    "src": "img/twitter.png"
+    "link": "https://projects.capradio.org/california-fire-history",
+    "title": "California Fire History",
+    "desc": "This is an interactive map to show wildfire history.",
+    "src": "img/fire.png"
 },{
-    "link": "https://github.com/witcheswhocode",
-    "title": "Github",
+    "link": "https://github.com/witcheswhocode/personal-links",
+    "title": "Code for This Website",
+    "desc": "This was built by me, fork the repo and customize your own!",
     "src": "img/github.png"
 },{
-    "link": "https://tiktok.com/@witcheswhocode",
-    "title": "TikTok",
-    "src": "img/tiktok.png"
-},{
-    "link": "https://tiktok.com/@witcheswhocode",
-    "title": "TikTok",
-    "src": "img/tiktok.png"
-},{
-    "link": "https://tiktok.com/@witcheswhocode",
-    "title": "TikTok",
-    "src": "img/tiktok.png"
-},{
-    "link": "https://tiktok.com/@witcheswhocode",
-    "title": "TikTok",
+    "link": "https://www.tiktok.com/@witcheswhocode/video/7071307087178976558",
+    "title": "Fried Potato Balls",
+    "desc": "TikTok of me making delicious food from leftovers.",
     "src": "img/tiktok.png"
 }];
 const socials = [{
@@ -43,6 +34,7 @@ var target = document.querySelector('#links');
 links.forEach(element => {
     var a = document.createElement('a');
     var button = document.createElement('button');
+    var p = document.createElement('p');
     var icon = document.createElement('img');
 
     // Add text to the new element
@@ -52,8 +44,10 @@ links.forEach(element => {
     icon.classList.add("link-icon");
     button.innerHTML = element.title;
     button.classList.add("link-button");
+    p.innerHTML = element.desc;
 
     // Insert the element before our target element
+    button.appendChild(p);
     a.appendChild(icon);
     a.appendChild( button );
     target.appendChild( a );
